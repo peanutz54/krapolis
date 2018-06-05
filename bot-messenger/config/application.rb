776f09eecda3ca4fc815ed54de2eed81278b6d65
mahoudeau.ca <mahoudeau.ca@gmail.com>
2018-06-05 06:16:26 -0400

    gemfile improved3

--- bot-messenger/config/application.rb
+++ bot-messenger/config/application.rb
@@ -19,9 +19,8 @@ module BotMessenger
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    # config/application.rb
-    config.assets.initialize_on_precompile = false
     config.paths.add File.join('app', 'bot'), glob: File.join('**', '*.rb')
     config.autoload_paths += Dir[Rails.root.join('app', 'bot', '*')]
+    
   end
 end
